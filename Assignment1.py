7bc47dd09bb7180dd85d22335ba65213c7355c2a
samson247 <50429870+samson247@users.noreply.github.com>
2020-09-22 14:10:24 -0500

    Add files via upload

--- Assignment1.py
+++ Assignment1.py
@@ -6,32 +6,26 @@ by removing the start codon, translate it according to the standard
 genetic code, and print the resulting amino acid sequence to the screen.
 
 '''
-import GenetiCode as gene
+import GenetiCode as genetic_code
+import KyteDoolittleValues as hydropathy_indices
+import HelixPropensities as helix_propensities
 
 
 # Converts RNA sequences to amino acid sequences and returns the result as a string
 def rna2aa(rna):
-    # replace the following line with the real code of this function
-    # get the amino acid dictionary.
-    # This is the way to access dictionary elements.
-    # print(gene.code["UUG"])
-    # print("Debugger for dict")
-    # print(gene.code[rna[3:6]])
-    # print("Debugger from rna2aa method")
-    # print(rna[0:10])
     not_finished = True
     amino_acid = ''
     index = 0
     while not_finished:
         base_pair = rna[index: index + 3]
         # Check if the length of base pair is 3 or not.
-        if len(base_pair) < 3:
+        if len(base_pair) < 3 or base_pair in ["UAA", "UAG", "UGA"]:
             # We have finished reading the sequence.
             not_finished = False
         else:
             # We have to check through the dictionary for matching
             # the rna to amino acid sequence.
-            amino_acid = amino_acid + gene.code[base_pair]
+            amino_acid = amino_acid + genetic_code.code[base_pair]
         index += 3
     return amino_acid
 
@@ -47,15 +41,80 @@ def read_file(filename):
     return genes
 
 
+# Prints the results of the hydrophobicity scan, including first and last indices, amino acid sequence,
+# and average scores for a window when they are greater than 1.6
+def print_hydrophobicity_results(results):
+    print "Hydrophobicity Results (Score > 1.6 signifies potential transmembrane domain)"
+    if not results:
+        print "No scores > 1.6 were found"
+    else:
+        print "| Beg | End |     AA Sequence     | Score |"
+        print "-------------------------------------------"
+        for result in results:
+            output = "| "
+            for x in result:
+                output += x
+                output += " | "
+            print output
+    print "\n"
+
+
+# Prints the results of the helix propensity scan, including first and last indices, amino acid sequence,
+# and average scores for a window when they are less than 0.4
+def print_helix_propensity_results(results):
+    print "Helix Propensity Results (Score < 0.4)"
+    if not results:
+        print "No scores < 0.4 were found"
+    else:
+        print "| Beg | End |     AA Sequence     | Score |"
+        print "-------------------------------------------"
+        for result in results:
+            output = "| "
+            for x in result:
+                output += x
+                output += " | "
+            print output
+    print "\n"
+
+
+# Scans the amino acid sequence for hydrophobicity and helix propensity
+def scan_sequence(aa_sequence):
+    window_size = 19
+    hydropathy_score = 0
+    helix_propensity_score = 0
+    hydropathy_results = []
+    helix_propensity_results = []
+
+    start_index = 0
+    while start_index < (len(aa_sequence) - window_size):
+        aa_subsequence = aa_sequence[start_index: start_index + window_size]
+        for aa in aa_subsequence:
+            hydropathy_score += hydropathy_indices.indices[aa]
+            helix_propensity_score += helix_propensities.helix_propensities[aa]
+        if (hydropathy_score / window_size) > 1.6:
+            hydropathy_results.append([str(start_index), str(start_index + (window_size - 1)), aa_subsequence,
+                                             str(round(hydropathy_score / window_size, 3))])
+        if (helix_propensity_score / window_size) < 0.4:
+            helix_propensity_results.append([str(start_index), str(start_index + (window_size - 1)), aa_subsequence,
+                                             str(round(helix_propensity_score / window_size, 3))])
+        start_index += 1
+        hydropathy_score = 0
+        helix_propensity_score = 0
+
+    print_hydrophobicity_results(hydropathy_results)
+    print_helix_propensity_results(helix_propensity_results)
+
+
 # Calls methods to read gene file and convert RNA to Amino Acids and prints the results
 def main():
     genes = read_file("Assignment1Sequences.txt")
 
-    counter = 0
+    counter = 1
     for rna in genes:
-        print("Sequence " + str(counter) + ": " + rna)
+        #print("Sequence " + str(counter) + ": " + rna)
         aa_sequence = rna2aa(rna)
         print("The resulting amino acid sequence for gene " + str(counter) + ": " + aa_sequence + "\n")
+        scan_sequence(aa_sequence)
         counter += 1
 
 
---
+++ HelixPropensities.py
@@ -0,0 +1,23 @@
+# A fragment of code that stores the helix propensity of amino acids in a dictionary
+
+helix_propensities = {
+    'A': 0,
+    'R': 0.21,
+    'N': 0.65,
+    'D': 0.69,
+    'C': 0.68,
+    'Q': 0.39,
+    'E': 0.40,
+    'G': 1,
+    'H': 0.61,
+    'I': 0.41,
+    'L': 0.21,
+    'K': 0.26,
+    'M': 0.24,
+    'F': 0.54,
+    'P': 1,
+    'S': 0.5,
+    'T': 0.66,
+    'W': 0.49,
+    'Y': 0.53,
+    'V': 0.61}
